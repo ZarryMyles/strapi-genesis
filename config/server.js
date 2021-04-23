e788dd8c51ca3d7c39d052562266facc54efb4e4
Mahanth <mahanth2709@gmail.com>
2021-04-23 10:24:48 +0530

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2d921dc28d53f53948df2b0ec7670e2d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cc0209ce-a0c3-4acd-a877-bb505f54db12'
+};
